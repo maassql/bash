a54053d93c960fb801232c49dafc0f24e15abb4f
jeff maass <jmaass@cccis.com>
2016-10-13 12:48:00 -0500

    less busy prompt setup

--- prompt/install_launch.sh
+++ prompt/install_launch.sh
@@ -1,8 +1,5 @@
-if [ -f "install_prompt2.sh" ] 
-then
-	rm "install_prompt2.sh"
-fi
-wget "https://raw.githubusercontent.com/maassql/bash/master/prompt/install_prompt2.sh"
+if [ -f "install_prompt2.sh" ] then rm "install_prompt2.sh" fi
+wget --quiet "https://raw.githubusercontent.com/maassql/bash/master/prompt/install_prompt2.sh"
 bash "install_prompt2.sh"
 rm "install_prompt2.sh"
 source "${HOME}/.jeff_maass_prompt/jmaass_prompt.sh"
--- prompt/install_prompt2.sh
+++ prompt/install_prompt2.sh
@@ -9,29 +9,22 @@ function set_prompt_directory(){
 
 function get_from_repository(){
         local to="${HOME_OF_PROMPT_SCRIPTS}/${1}"
-        echo "get_from_repository=[${1}]"
-        echo "to=[${to}]"
         
         cd "${HOME_OF_PROMPT_SCRIPTS}"
         
         if [ -f "${to}" ] 
        	then
-       		echo "removing old copy of to=[${to}]."
             rm "${to}"
         fi
 
-        echo "pulling down [${1}]"
         wget ${WGET_OPTS} "https://raw.githubusercontent.com/maassql/bash/master/prompt/${1}"
-
-        echo "sourcing to=[${1}]"
-        source "${1}"
 }
 
 
 
 HOME_OF_PROMPT_SCRIPTS=$(set_prompt_directory)
 WGET_LOG="${HOME_OF_PROMPT_SCRIPTS}/wget-log.log"
-WGET_OPTS="--tries=2 --no-verbose --show-progress --dns-timeout=2 --read-timeout=2 --connect-timeout=4 --prefer-family=IPv4 --no-dns-cache --retry-connrefused "
+WGET_OPTS="--quiet --tries=2 --no-verbose --show-progress --dns-timeout=2 --read-timeout=2 --connect-timeout=4 --prefer-family=IPv4 --no-dns-cache --retry-connrefused "
 
 
 get_from_repository 'jmaass_prompt.sh'
