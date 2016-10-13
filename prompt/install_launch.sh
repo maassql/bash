71763996f02c48cce6408946d0654b1ca61918e3
jeff maass <jmaass@cccis.com>
2016-10-13 12:09:14 -0500

    bug

--- prompt/install_launch.sh
+++ prompt/install_launch.sh
@@ -4,7 +4,8 @@
 #		Regular Expression		=Last login:
 #       Action               	=Send Text
 #       Parameters              ={the code below}
-if [ -f "install_prompt2.sh" ]; then
+if [ -f "install_prompt2.sh" ] 
+then
 	rm "install_prompt2.sh"
 fi
 wget "https://raw.githubusercontent.com/maassql/bash/master/prompt/install_prompt2.sh"
--- prompt/install_prompt2.sh
+++ prompt/install_prompt2.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 function set_prompt_directory(){
-	cd
-	local PROMPT_HOME=$(pwd)/.jeff_maass_prompt/
-	mkdir -p "${PROMPT_HOME}"
-	cd "${PROMPT_HOME}"
-	echo "${PROMPT_HOME}"
+        cd
+        local PROMPT_HOME=$(pwd)/.jeff_maass_prompt/
+        mkdir -p "${PROMPT_HOME}"
+        cd "${PROMPT_HOME}"
+        echo "${PROMPT_HOME}"
 }
 
 function get_from_repository(){
-	local to="${HOME_OF_PROMPT_SCRIPTS}/${1}"
-	
-	if [ -f "${to}" ] then;
-		rm "${to}"
-	fi
+        local to="${HOME_OF_PROMPT_SCRIPTS}/${1}"
+        echo "get_from_repository=[${to}]"
+        if [ -f "${to}" ] 
+       	then
+                rm "${to}"
+        fi
 
-	wget ${WGET_OPTS} "${1}"
-	
-	source "${to}"
+        wget ${WGET_OPTS} "${1}"
+
+        source "${to}"
 }
 
 WGET_OPTS="--output-file=\"${WGET_LOG}\" --tries=2 --no-verbose --base=\"${REPO}\" --show-progress --dns-timeout=2 --read-timeout=2 --connect-timeout=4 --prefer-family=IPv4 --no-dns-cache --retry-connrefused "
