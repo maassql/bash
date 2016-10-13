f32d920abc86b34262f2f4fffa8b3f0029a05419
jeff maass <jmaass@cccis.com>
2016-10-13 12:41:54 -0500

    working on my mac

---
+++ prompt/README.md
@@ -0,0 +1,6 @@
+# paste this into your command line
+# ---or---
+# put it into an iterm2.trigger.Parameter field :
+#		Regular Expression		=Last login:
+#       Action               	=Send Text
+#       Parameters              ={the code in the file, install_launch.sh}
--- prompt/install_launch.sh
+++ prompt/install_launch.sh
@@ -1,9 +1,3 @@
-# paste this into your command line
-# ---or---
-# put it into an iterm2.trigger.Parameter field :
-#		Regular Expression		=Last login:
-#       Action               	=Send Text
-#       Parameters              ={the code below}
 if [ -f "install_prompt2.sh" ] 
 then
 	rm "install_prompt2.sh"
@@ -11,4 +5,4 @@ fi
 wget "https://raw.githubusercontent.com/maassql/bash/master/prompt/install_prompt2.sh"
 bash "install_prompt2.sh"
 rm "install_prompt2.sh"
-source "${HOME_OF_PROMPT_SCRIPTS}/jmaass_prompt.sh"
+source "${HOME}/.jeff_maass_prompt/jmaass_prompt.sh"
