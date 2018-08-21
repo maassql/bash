ebcc5b559118992018b1ff3f6527c3f75db97502
jeffery maass <jmaass@cccis.com>
2018-08-21 13:05:55 -0500

    comment deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 
+# ----------------------------------------------------------------------
+# ------ Get latest code from remote repository ------------------------
 repo_dir="$HOME/bash"
-
 if [ -d "${repo_dir}" ];
     then
         echo "Pulling from remote git repository to local directory, [${repo_dir}]."
@@ -11,9 +12,21 @@ if [ -d "${repo_dir}" ];
         cd $HOME
         git clone https://github.com/maassql/bash
 fi
+# ------ Get latest code from remote repository ------------------------
+# ----------------------------------------------------------------------
 
+
+# ----------------------------------------------------------------------
+# ---- Install prompt to your home directory ---------------------------
 bash "${repo_dir}"/prompt_2017/repo_to_home.sh
+# ---- Install prompt to your home directory ---------------------------
+# ----------------------------------------------------------------------
 
+
+# ----------------------------------------------------------------------
+# ----- show the user their prompt has changed -------------------------
 echo "BEGIN - Sourcing prompt scripts."
 source $HOME/jmaass_prompt.sh
 echo "DONE - Sourcing prompt scripts."
+# ----- show the user their prompt has changed -------------------------
+# ----------------------------------------------------------------------
